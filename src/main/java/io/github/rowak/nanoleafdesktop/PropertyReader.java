840f4470575eada941a7a63b3123d7c9e7551d8b
twinkybot <erwin.yuekselgil@valtech.com>
2020-06-04 15:53:52 +0200

    creating missing directory

--- src/main/java/io/github/rowak/nanoleafdesktop/PropertyReader.java
+++ src/main/java/io/github/rowak/nanoleafdesktop/PropertyReader.java
@@ -5,32 +5,33 @@ import java.io.Serializable;
 
 public class PropertyReader implements Serializable {
     public String getPropertiesFilePath() {
-        String userHome = "";
-        String infix = "";
         String applicationName = "Nanoleaf for Desktop";
         String propertyFileName = "preferences.txt";
 
-        String dir = "";
-        final String os = getOS();
-
-        if (os.contains("win")) {
-            userHome = System.getenv("APPDATA") + File.separator;
-        } else if (os.contains("mac")) {
-            userHome = System.getProperty("user.home") + File.separator;
-            infix = "Library" + File.separator + "Application Support" + File.separator;
-        } else if (os.contains("nux")) {
-            userHome = System.getProperty("user.home") + File.separator;
-            infix = ".";
-        }
-
-        dir = userHome + infix + applicationName;
+        String prefix = getOSDependentPrefix();
+        String dir = prefix + applicationName;
 
         create(dir);
 
         return dir + File.separator + propertyFileName;
     }
 
-    protected void create(String dir) {
+    protected String getOSDependentPrefix() {
+        String os = getOS();
+        String prefix = "";
+
+        if (os.contains("win")) {
+            prefix = System.getenv("APPDATA") + File.separator;
+        } else if (os.contains("mac")) {
+            prefix = System.getProperty("user.home") + File.separator + "Library" + File.separator + "Application Support" + File.separator;
+        } else if (os.contains("nux")) {
+            prefix = System.getProperty("user.home") + File.separator + ".";
+        }
+
+        return prefix;
+    }
+
+    private void create(String dir) {
         File dirFile = new File(dir);
 
         if (!dirFile.exists()) {
--- src/test/java/io/github/rowak/nanoleafdesktop/PropertyReaderShould.java
+++ src/test/java/io/github/rowak/nanoleafdesktop/PropertyReaderShould.java
@@ -1,16 +1,29 @@
 package io.github.rowak.nanoleafdesktop;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PropertyReaderShould {
 
+    @Rule
+    public TemporaryFolder folderForPreferenceFile = new TemporaryFolder();
+
     public final String applicationName = "Nanoleaf for Desktop";
     private String userHome;
     private String propertyFileName = "preferences.txt";
+    private File tempFolder;
+
+    @Before
+    public void setUp() throws Exception {
+        tempFolder = folderForPreferenceFile.newFolder();
+    }
 
     @Test
     public void determine_property_file_path_for_windows() {
@@ -33,7 +46,6 @@ public class PropertyReaderShould {
         assertThat(propertyFilePathForMac).isEqualTo(userHome + File.separator + macAppLibraryPath + File.separator + applicationName + File.separator + propertyFileName);
     }
 
-
     @Test
     public void determine_propert_file_path_for_linux() {
         userHome = System.getProperty("user.home");
@@ -44,25 +56,44 @@ public class PropertyReaderShould {
         assertThat(propertyFilePathForNix).isEqualTo(userHome + File.separator + hiddenFilePrefix + applicationName + File.separator + propertyFileName);
     }
 
+    @Test
+    public void create_folder_for_preference_file() throws IOException {
+        assertThat(tempFolder).isEmptyDirectory();
+
+        String propertyFilePath = new TestablePropertyReader().getPropertiesFilePath();
+
+        File propertyFile = new File(propertyFilePath);
+        File propertyFileParentFolder = propertyFile.getParentFile();
+        assertThat(propertyFile).doesNotExist();
+        assertThat(propertyFileParentFolder).exists();
+    }
+
     private class TestablePropertyReaderForWindows extends PropertyReader {
 
         @Override
-        protected String getOs() {
-            return "win";
+        protected String getOSDependentPrefix() {
+            return System.getenv("APPDATA") + File.separator;
         }
     }
 
     private class TestablePropertyReaderForMac extends PropertyReader {
         @Override
-        protected String getOs() {
-            return "mac";
+        protected String getOSDependentPrefix() {
+            return System.getProperty("user.home") + File.separator + "Library" + File.separator + "Application Support" + File.separator;
         }
     }
 
     private class TestablePropertyReaderForLinux extends PropertyReader {
         @Override
-        protected String getOs() {
-            return "nux";
+        protected String getOSDependentPrefix() {
+            return System.getProperty("user.home") + File.separator + ".";
+        }
+    }
+
+    private class TestablePropertyReader extends PropertyReader {
+        @Override
+        protected String getOSDependentPrefix() {
+            return tempFolder.getPath() + File.separator;
         }
     }
 }
