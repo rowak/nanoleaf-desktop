a6714a91323f8b4f9ccf4b42608788cc863c44c7
twinkybot <erwin.yuekselgil@valtech.com>
2020-06-04 14:11:40 +0200

    First step extracting PropertyReader

--- src/test/java/io/github/rowak/nanoleafdesktop/PropertyReaderShould.java
+++ src/test/java/io/github/rowak/nanoleafdesktop/PropertyReaderShould.java
@@ -8,13 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PropertyReaderShould {
 
-    public static final String appData = System.getenv("APPDATA");
-
     @Test
     public void determine_property_file_path_for_windows() {
         String propertyFilePathForWindows = new TestablePropertyReaderForWindows().getPropertiesFilePath();
 
-        assertThat(propertyFilePathForWindows).isEqualTo(appData + File.separator + "Nanoleaf for Desktop" + File.separator + "preferences.txt");
+        //TODO should be preferred
+        //assertThat(propertyFilePathForWindows).isEqualTo(System.getProperty("user.home") + File.separator + "Nanoleaf for Desktop" + File.separator + "preferences.txt");
+        assertThat(propertyFilePathForWindows).isEqualTo(System.getenv("APPDATA") + File.separator + "Nanoleaf for Desktop" + File.separator + "preferences.txt");
     }
 
     private class TestablePropertyReaderForWindows extends PropertyReader {
